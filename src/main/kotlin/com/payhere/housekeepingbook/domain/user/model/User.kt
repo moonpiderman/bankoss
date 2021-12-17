d7efbdcff1b02c1f6274a96185cace2a05545d9b
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 16:35:47 +0900

    Add: User Model

---
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/user/model/User.kt
@@ -0,0 +1,22 @@
+package com.payhere.housekeepingbook.domain.user.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.payhere.housekeepingbook.domain.model.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.validation.constraints.NotBlank
+
+@Entity
+@Table(name = "user")
+class User(
+    @Column
+    @field:NotBlank
+    val email: String,
+
+    @field:NotBlank
+    var password: String,
+
+    @JsonIgnore
+    val authorities: String = "User",
+): BaseEntity()
