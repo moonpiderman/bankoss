1ba9450b314beda3782853245e30f3570debdc0e
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 16:52:15 +0900

    Fix: Setting Security

--- src/main/kotlin/com/payhere/housekeepingbook/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/global/config/SecurityConfig.kt
@@ -1,7 +1,61 @@
 package com.payhere.housekeepingbook.global.config
 
+import com.payhere.housekeepingbook.global.auth.SigninAuthenticationFilter
+import com.payhere.housekeepingbook.global.auth.jwt.JwtAuthenticationEntryPoint
+import com.payhere.housekeepingbook.global.auth.jwt.JwtAuthenticationFilter
+import com.payhere.housekeepingbook.global.auth.jwt.JwtTokenProvider
+import com.payhere.housekeepingbook.global.auth.model.UserPrincipalDetailService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
-class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    private val userPrincipalDetailService: UserPrincipalDetailService,
+) : WebSecurityConfigurerAdapter() {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun daoAuthenticationProvider(): DaoAuthenticationProvider {
+        val provider = DaoAuthenticationProvider()
+        provider.setPasswordEncoder(passwordEncoder())
+        provider.setUserDetailsService(userPrincipalDetailService)
+        return provider
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth
+            .authenticationProvider(daoAuthenticationProvider())
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .and()
+            .addFilter(SigninAuthenticationFilter(authenticationManager(), jwtTokenProvider))
+            .addFilter(JwtAuthenticationFilter(authenticationManager(), jwtTokenProvider))
+            .authorizeRequests()
+            .antMatchers("/api/user/signin/").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/user/signup/").anonymous()
+            .anyRequest().authenticated()
+    }
 }
