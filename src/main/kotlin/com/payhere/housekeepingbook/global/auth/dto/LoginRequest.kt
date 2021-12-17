1aec9677a4ed0fc976d67905ec3266d167e6c665
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 16:49:24 +0900

    Fix: Spell

--- src/main/kotlin/com/payhere/housekeepingbook/global/auth/dto/LoginRequest.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/global/auth/dto/LoginRequest.kt
@@ -1,6 +1,6 @@
 package com.payhere.housekeepingbook.global.auth.dto
 
 class LoginRequest(
-    val eamil: String? = null,
-    val passwrod: String? = null,
+    val email: String? = null,
+    val password: String? = null,
 )
