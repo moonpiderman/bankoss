2288e88849a1bce9d7ea9043db4dc060b53e75fd
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 20:09:40 +0900

    Add: User Log out

--- src/main/kotlin/com/payhere/housekeepingbook/domain/user/api/UserController.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/user/api/UserController.kt
@@ -5,10 +5,14 @@ import com.payhere.housekeepingbook.domain.user.service.UserService
 import com.payhere.housekeepingbook.global.auth.jwt.JwtTokenProvider
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
@@ -26,5 +30,15 @@ class UserController(
         val user = userService.signup(signupRequest)
         response.addHeader("Authentication", user.accessToken)
         return ResponseEntity(UserDto.Response(user), HttpStatus.CREATED)
+    }
+
+    @PostMapping("/logout/")
+    fun userLogout(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+    ): ResponseEntity<UserDto.Response> {
+        val auth: Authentication = SecurityContextHolder.getContext().authentication
+        if (auth != null) SecurityContextLogoutHandler().logout(request, response, auth)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
