d099ac3ce92af0194500eb8c4c8754d3845550bb
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 18:06:04 +0900

    Add: User Sign Up

--- src/main/kotlin/com/payhere/housekeepingbook/domain/user/api/UserController.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/user/api/UserController.kt
@@ -3,11 +3,13 @@ package com.payhere.housekeepingbook.domain.user.api
 import com.payhere.housekeepingbook.domain.user.dto.UserDto
 import com.payhere.housekeepingbook.domain.user.service.UserService
 import com.payhere.housekeepingbook.global.auth.jwt.JwtTokenProvider
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
 @RestController
@@ -16,4 +18,13 @@ class UserController(
     private val jwtTokenProvider: JwtTokenProvider,
     private val userService: UserService,
 ) {
+    @PostMapping("/signup/")
+    fun userSignup(
+        @Valid @RequestBody signupRequest: UserDto.SignupRequest,
+        response: HttpServletResponse,
+    ): ResponseEntity<UserDto.Response> {
+        val user = userService.signup(signupRequest)
+        response.addHeader("Authentication", jwtTokenProvider.generateToken(user.email))
+        return ResponseEntity(UserDto.Response(user), HttpStatus.CREATED)
+    }
 }
