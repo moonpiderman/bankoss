46531fe3f64706e947e22b9b492f378deb60753e
moonpiderman <lotsofchuck@gmail.com>
2021-12-17 15:41:58 +0900

    Init

---
+++ src/main/kotlin/com/payhere/housekeepingbook/HousekeepingbookApplication.kt
@@ -0,0 +1,11 @@
+package com.payhere.housekeepingbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HousekeepingbookApplication
+
+fun main(args: Array<String>) {
+	runApplication<HousekeepingbookApplication>(*args)
+}
