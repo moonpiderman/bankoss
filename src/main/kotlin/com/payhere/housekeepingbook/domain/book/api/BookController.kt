1ab4f9a968c3f9fc85871a0f620097c78d268c1d
moonpiderman <lotsofchuck@gmail.com>
2021-12-19 12:25:59 +0900

    Fix: api, repository

--- src/main/kotlin/com/payhere/housekeepingbook/domain/book/api/BookController.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/book/api/BookController.kt
@@ -1,8 +1,11 @@
 package com.payhere.housekeepingbook.domain.book.api
 
+import com.payhere.housekeepingbook.domain.book.repository.BookRepository
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/book")
-class BookController()
+class BookController(
+    private val bookRepository: BookRepository,
+)
--- src/main/kotlin/com/payhere/housekeepingbook/domain/book/repository/BookRepository.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/book/repository/BookRepository.kt
@@ -1,3 +1,6 @@
 package com.payhere.housekeepingbook.domain.book.repository
 
-interface BookRepository
+import com.payhere.housekeepingbook.domain.book.model.Book
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BookRepository : JpaRepository<Book, Long?>
