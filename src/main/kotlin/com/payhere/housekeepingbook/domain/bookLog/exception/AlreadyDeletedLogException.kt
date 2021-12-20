ba7f30f7f7fb2a209b7715b8c0b0bbbda296d94e
moonpiderman <lotsofchuck@gmail.com>
2021-12-20 15:33:10 +0900

    Add: Restore Log

--- src/main/kotlin/com/payhere/housekeepingbook/domain/bookLog/exception/AlreadyDeletedLogException.kt
+++ src/main/kotlin/com/payhere/housekeepingbook/domain/bookLog/exception/AlreadyDeletedLogException.kt
@@ -3,5 +3,5 @@ package com.payhere.housekeepingbook.domain.bookLog.exception
 import com.payhere.housekeepingbook.global.exception.ConflictException
 import com.payhere.housekeepingbook.global.exception.ErrorType
 
-class AlreadyDeletedLog(detail: String = "") :
+class AlreadyDeletedLogException(detail: String = "") :
     ConflictException(ErrorType.ALREADY_DELETED, detail)
