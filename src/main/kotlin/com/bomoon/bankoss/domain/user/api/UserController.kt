9d2ab8611a48b0e9ac6fc02983d3a4c6a2cdf591
moonpiderman <lotsofchuck@gmail.com>
2022-01-13 20:53:38 +0900

    Refactor: UserController

--- src/main/kotlin/com/bomoon/bankoss/domain/user/api/UserController.kt
+++ src/main/kotlin/com/bomoon/bankoss/domain/user/api/UserController.kt
@@ -6,7 +6,6 @@ import com.bomoon.bankoss.domain.user.service.UserService
 import com.bomoon.bankoss.global.auth.CurrentUser
 import com.bomoon.bankoss.global.auth.jwt.JwtTokenProvider
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -26,29 +26,31 @@ class UserController(
     private val userService: UserService,
 ) {
     @PostMapping("/signup/")
+    @ResponseStatus(HttpStatus.CREATED)
     fun userSignup(
         @Valid @RequestBody signupRequest: UserDto.SignupRequest,
         response: HttpServletResponse,
-    ): ResponseEntity<UserDto.Response> {
+    ): UserDto.Response {
         val user = userService.signup(signupRequest)
         response.addHeader("Authentication", user.accessToken)
-        return ResponseEntity(UserDto.Response(user), HttpStatus.CREATED)
+        return UserDto.Response(user)
     }
 
     @PostMapping("/logout/")
+    @ResponseStatus(HttpStatus.OK)
     fun userLogout(
         request: HttpServletRequest,
         response: HttpServletResponse,
-    ): ResponseEntity<UserDto.Response> {
+    ) {
         val auth: Authentication = SecurityContextHolder.getContext().authentication
         if (auth != null) SecurityContextLogoutHandler().logout(request, response, auth)
-        return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("/me/")
+    @ResponseStatus(HttpStatus.OK)
     fun viewBooks(
         @CurrentUser user: User,
-    ): ResponseEntity<UserDto.UserBooksResponse> {
-        return ResponseEntity(UserDto.UserBooksResponse(user), HttpStatus.OK)
+    ): UserDto.UserBooksResponse {
+        return UserDto.UserBooksResponse(user)
     }
 }
