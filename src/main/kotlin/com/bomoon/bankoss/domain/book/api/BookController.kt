da1ce452151a508bd2097a76b94fcff855adb8cb
moonpiderman <lotsofchuck@gmail.com>
2022-02-05 16:45:27 +0900

    Feat: Get this book

--- src/main/kotlin/com/bomoon/bankoss/domain/book/api/BookController.kt
+++ src/main/kotlin/com/bomoon/bankoss/domain/book/api/BookController.kt
@@ -2,10 +2,11 @@ package com.bomoon.bankoss.domain.book.api
 
 import com.bomoon.bankoss.domain.book.dto.BookDto
 import com.bomoon.bankoss.domain.book.service.BookService
-import com.bomoon.bankoss.domain.bookLog.service.BookLogService
 import com.bomoon.bankoss.domain.user.model.User
 import com.bomoon.bankoss.global.auth.CurrentUser
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,7 +18,6 @@ import javax.validation.Valid
 @RequestMapping("/api/book")
 class BookController(
     private val bookService: BookService,
-    private val bookLogService: BookLogService,
 ) {
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,5 +26,15 @@ class BookController(
         @Valid @RequestBody createRequest: BookDto.CreateRequest
     ): BookDto.BookResponse {
         return bookService.createBook(user, createRequest)
+    }
+
+    @GetMapping("/{book_id}/")
+    @ResponseStatus(HttpStatus.OK)
+    fun getBook(
+        @CurrentUser user: User,
+        @PathVariable("book_id") bookId: Long,
+    ): BookDto.BookResponse {
+        val thisBook = bookService.getThisBook(bookId)
+        return BookDto.BookResponse(thisBook)
     }
 }
